980bcb226012b3d74cf46be9852a0be0f5b0ef00
Moataz Mohammady <moataz@wuilt.com>
2023-08-21 14:33:22 +0300

    set backgroundImagePath

--- countdown.js
+++ countdown.js
@@ -1,10 +1,10 @@
-const { createCanvas } = require("canvas");
+const { createCanvas, loadImage } = require("canvas");
 const GIFEncoder = require("gifencoder");
 const fs = require("fs");
 
-function generateCountdownGif(targetDate, outputFile) {
-  const canvasWidth = 600;
-  const canvasHeight = 200;
+function generateCountdownGif(targetDate, backgroundImage, outputFile) {
+  const canvasWidth = 400;
+  const canvasHeight = 100;
 
   const canvas = createCanvas(canvasWidth, canvasHeight);
   const ctx = canvas.getContext("2d");
@@ -16,11 +16,7 @@ function generateCountdownGif(targetDate, outputFile) {
   encoder.start();
   encoder.setRepeat(0); // Repeat forever
   encoder.setDelay(1000); // 1 frame per second
-  encoder.setQuality(10); // Lower quality to reduce file size
-
-  // Set the canvas background color to transparent
-  ctx.fillStyle = "rgba(0, 0, 0, 0)";
-  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  encoder.setQuality(10);
 
   const updateCountdown = () => {
     const currentTime = new Date();
@@ -33,10 +29,15 @@ function generateCountdownGif(targetDate, outputFile) {
 
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    ctx.fillStyle = "white";
+    // Draw the background image
+    ctx.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
+
+    // Set the text color to black
+    ctx.fillStyle = "black";
     ctx.font = "bold 48px Arial";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+
     ctx.fillText(
       `${days}d ${hours}h ${minutes}m ${seconds}s`,
       canvasWidth / 2,
@@ -58,6 +59,10 @@ function generateCountdownGif(targetDate, outputFile) {
   updateCountdown();
 }
 
-// Usage example: generate a countdown GIF to December 31, 2023, 12:00:00 UTC
+// Usage example: generate a countdown GIF to August 22, 2023, 12:00:00 UTC with a background image
 const targetDate = new Date("2023-08-22T12:00:00Z");
-generateCountdownGif(targetDate, "countdown.gif");
+const backgroundImagePath =
+  "https://placehold.co/400x100/FFFFFF/000000?text=\\n"; // Replace with the path to your background image
+loadImage(backgroundImagePath).then((backgroundImage) => {
+  generateCountdownGif(targetDate, backgroundImage, "countdown.gif");
+});
